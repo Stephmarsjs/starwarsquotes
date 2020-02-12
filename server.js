f204e2ec521065bd73de39bcbf9204be552bcc42
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-02-11 23:43:13 -0500

    added body parser

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log('Hellooooo thereeeeee!!!!!')
 })
 
 app.listen(3000, function() {
