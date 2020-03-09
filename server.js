6569b1c39f2e508aeb122ef4c058f701b3edcee3
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-09 03:51:16 -0400

    removed routes from server.js file

--- server.js
+++ server.js
@@ -3,29 +3,9 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.get('/', (req,res) => {
-    const crusor = db.collection('quotes').find()
-})
-
-app.post('/quotes', (req, res) => {
-    db.collection('quotes').insertOne(req.body, (err, result) => 
-    {
-        if (err) return console.log(err)
-        
-        console.log('saved to database')
-        res.redirect('/')
-    })
-})
-
 var db 
 
-MongoClient.connect('mongodb+srv://<blank>:<password>@starwarscluster-19fwm.mongodb.net/test?retryWrites=true&w=majority',
+MongoClient.connect('mongodb+srv://stephmarsjs:Franklin1223@starwarscluster-19fwm.mongodb.net/test?retryWrites=true&w=majority',
 { useUnifiedTopology: true }, (err, client) => {
     if (err) return console.log(err)
     db = client.db('starwarscluster') // whatever your database name is
