53d1bd244460589bdff159817f55ffacfb93e76f
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-09 03:11:39 -0400

    post route to redirect user somewhere

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    db.collection('quotes').insertOne(req.body, (err, result) => 
+    {
+        if (err) return console.log(err)
+        
+        console.log('saved to database')
+        res.redirect('/')
+    })
 })
 
 var db 
