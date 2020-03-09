39f67ba860ba0b9cdf8ad2044ecf6008bab2299e
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-03-09 04:11:02 -0400

    added ejs file

---
+++ routes/quotes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', "ejs");
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/', (req,res) => {
+    const crusor = db.collection('quotes').find().toArray(function(err, results)
+    {
+        console.log(results)
+    })
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').insertOne(req.body, (err, result) => 
+    {
+        if (err) return console.log(err)
+        
+        console.log('saved to database')
+        res.redirect('/')
+
+        db.collection('quotes').find().toArray(function(err, results){
+            console.log(results)
+        })
+    })
+})
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').insertOne(req.body, (err, result) =>
+    {
+        if (err) return console.log(err)
+        console.log('save')
+    })
+})
